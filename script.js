f381e9fdf4061c33b9a603a9f139379237204984
hsattar <39243545+hsattar@users.noreply.github.com>
2021-09-30 15:18:00 +0100

    Added a Bingo Board for the user, need to ensure each number is different

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const generateBinogBoard = () => {
+const generateBinogBoard = (num) => {
     let bingoContainer = document.getElementsByClassName('bingo-container')[0]
-    for (let i = 1; i <= 76; i++) {
+    for (let i = 1; i <= num; i++) {
         let bingoNumber = document.createElement('div')
         bingoNumber.classList.add('bingo-number')
         bingoNumber.innerText = i
@@ -8,24 +8,34 @@ const generateBinogBoard = () => {
     }
 }
 
-generateBinogBoard()
+generateBinogBoard(76)
 
+let numbersGenerated = []
 
 const generateRandomNum = () => {
     let numberDisplay = document.getElementById('displayRandomNum')
-    let randomNum = Math.ceil(Math.random() * 76)    
+    let randomNum = Math.ceil(Math.random() * 76)
     numberDisplay.innerHTML = `<p>${randomNum}</p>`
+    numbersGenerated.push(randomNum)
     unHighlightRandomNum() 
     highlightRandomNum(randomNum)
 }
 
 let bingoNumbers = document.getElementsByClassName('bingo-number')
+let userNumbers = document.getElementsByClassName('user-number')
 
 const unHighlightRandomNum = () => {
     for (bingoNum of bingoNumbers) {
         for (classList of bingoNum.classList) {
             if (classList === 'highlighted') {
                 bingoNum.classList.remove('highlighted')
+            }
+        }
+    }
+    for (userNum of userNumbers) {
+        for (classList of userNum.classList) {
+            if (classList === 'highlighted') {
+                userNum.classList.remove('highlighted')
             }
         }
     }
@@ -37,11 +47,40 @@ const highlightRandomNum = (number) => {
             bingoNum.classList.add('highlighted')
         }
     }
+    for (let userNum of userNumbers) {
+        if (userNum.innerText == number) {
+            userNum.classList.add('highlighted')
+        }
+    }
 }
-
 
 let randonNumBtn = document.getElementById('numGenerator')
 randonNumBtn.addEventListener('click', generateRandomNum)
+
+const createUserBoard = () => {
+    let userContainer = document.getElementsByClassName('userboard-container')[0]
+    for (let i = 0; i <= 24; i++) {
+        let userBoard = document.createElement('div')
+        userBoard.classList.add('user-number')
+        let userRandomNum = Math.ceil(Math.random() * 76)
+        userBoard.innerText = userRandomNum
+        userContainer.appendChild(userBoard)
+    }
+}
+
+createUserBoard()
+
+// STORE RANDOM NUMBERS GENERATED INTO AN ARRAY
+// WHEN GENERATING A RANDOM NUMBER CHECK THE ARRAY
+// IF NUMBER ALREADY EXISTS THEN EXECUTE THE FUNCTION AGAIN
+
+
+// USER BOARD
+// CREATE A BOARD OF 24 NUMBERS
+// GENERATE RANDOM NUMBER FOR EACH SPACE
+// WHEN RANDOM NUMBER IS GENERATED CHECK IF NUMBER IS ON USER BOARD
+// IF NUMBER EXISTS ADD HIGHLIGHT CLASS
+// REMOVE CLASS AFTER
 
 
 // TASKS
